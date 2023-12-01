2f30507e6c001b4474e9c6f97c493c34a2845655
Masaki Gima <e235702@ie.u-ryukyu.ac.jp>
2023-12-02 01:23:41 +0900

    step2

--- src/jp/ac/uryukyu/ie/e235702/Main.java
+++ src/jp/ac/uryukyu/ie/e235702/Main.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e235702;
 
 public class Main{
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(e.getMessage());
+        }
     }
 }
